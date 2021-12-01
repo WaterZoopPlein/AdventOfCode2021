de4d9c0b644cd98cf785ad895ade3b6f51e153e1
Nguyen MK <28999049+WaterZoopPlein@users.noreply.github.com>
2021-12-01 12:57:17 +0000

    Add Core library and restructuring

---
+++ AoC2021/AoC2021Core/ReadInput.cs
@@ -0,0 +1,72 @@
+﻿namespace AoC2021Core
+{
+    public class ReadInput
+    {
+        public static List<int> ConvertInputTextToIntList(string path)
+        {
+            List<int> outputList = new ();
+
+            try
+            {
+                var sr = new StreamReader(path);
+
+                string? line;
+
+                while ((line = sr.ReadLine()) != null) outputList.Add(int.Parse(line));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred whilst loading {Path.GetFullPath(path)}.");
+                Console.WriteLine(e.Message);
+            }
+
+            return outputList;
+        }
+
+        public static List<long> ConvertInputTextToLongIntList(string path)
+        {
+            List<long> outputList = new();
+
+            try
+            {
+                var sr = new StreamReader(path);
+
+                string? line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    outputList.Add(long.Parse(line));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred whilst loading {Path.GetFullPath(path)}.");
+                Console.WriteLine(e.Message);
+            }
+
+            return outputList;
+        }
+
+        public static List<string> ConvertInputTextToStringList(string path, string[] delimiter)
+        {
+            List<string> outputList = new();
+
+            try
+            {
+                using var sr = new StreamReader(path);
+                outputList = new List<string>();
+
+                var textOutput = sr.ReadToEnd();
+
+                outputList = textOutput.Split(delimiter, StringSplitOptions.RemoveEmptyEntries).ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred whilst loading {Path.GetFullPath(path)}.");
+                Console.WriteLine(e.Message);
+            }
+
+            return outputList;
+        }
+    }
+}
---
+++ AoC2021/AoC2021Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using AoC2021Core;
+
+namespace AoC2021Main
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var test = ReadInput.ConvertInputTextToIntList(@"..\..\..\..\Inputs\Day01.txt");            
+        }
+    }
+}
